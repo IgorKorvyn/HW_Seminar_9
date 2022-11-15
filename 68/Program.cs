88b8b1ea689431d9890dc930d14a6068e196a7c9
Korvyn <korvyn555@google.com>
2022-11-15 20:10:42 +0300

    66

--- 68/Program.cs
+++ 68/Program.cs
@@ -5,3 +5,20 @@
 m = 2, n = 3->A(m, n) = 9
 m = 3, n = 2->A(m, n) = 29 */
 
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+    {
+        if (m > 0 && n == 0)
+            return Akkerman(m - 1, 1);
+        else
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Значение функции Аккермана ({m} , {n}) = {Akkerman(m, n)}");
