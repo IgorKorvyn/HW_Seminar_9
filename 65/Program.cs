1f014e37030575885028137aab05414f1a90f18a
Korvyn <korvyn555@google.com>
2022-11-15 19:57:14 +0300

    66

---
+++ 65/Program.cs
@@ -0,0 +1,29 @@
+﻿// 69 task через функцию
+
+/* Console.Clear();
+Console.Write("Введите число A  : ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num_n = num;    //число, возведенное в степень
+for (int i = 1; i < n; i++)
+{
+    num_n = num_n * num;
+}
+Console.Write("число A в степени B = ");
+Console.WriteLine(num_n); */
+
+string ResultNumbers(int m, int n)
+{
+    if (n == m)
+        return $"{m}";
+    return ResultNumbers(m, n - 1) + ", " + Convert.ToString(n);
+}
+
+
+Console.Clear();
+Console.Write("Введите число 1-ое: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2-ое: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ResultNumbers(m, n));
---
+++ 66/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Решать через рекурсию!!! (НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ) 
+Задача 66: 
+Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+int ResultNumbers(int m, int n)
+{
+    if (n == m) return n;
+
+    return n + ResultNumbers(m, n - 1);
+
+}
+
+Console.Clear();
+Console.Write("Введите число 1-ое: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2-ое: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(($"Cумма натуральных элементов между числами = {ResultNumbers(m, n)}"));
